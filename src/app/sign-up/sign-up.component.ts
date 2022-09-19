a40a7ceb8ce5f1e4acef57568583b8971e0405cd
lindajoy <wawirajoy98@gmail.com>
2022-09-19 20:38:57 +0300

    📦🎨 Sign Up Component

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  
+  registerForm!: FormGroup
+
+  constructor(private _fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this._initializeForm();
+
+  }
+
+  private _initializeForm(){
+    this.registerForm = this._fb.group({
+      name: ['', [Validators.required, Validators.maxLength(16)]],
+      password: ['', [Validators.required, Validators.maxLength(64)]],
+      email:['', [Validators.required, Validators.maxLength(64)]]
+    })
+  }
+
+}
