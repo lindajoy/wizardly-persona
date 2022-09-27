61a49a8fb9c8047dd6d0df35fa8ba082d5d85d4f
Paul Chesa <paulchesa1@gmail.com>
2022-09-27 21:25:32 +0300

    🚀Set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import  { SigninComponent } from './components/signin/signin.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-const routes: Routes = [];
+//Main routes module
+const routes: Routes = [
+  { path: 'home', component: LandingPageComponent },
+  { path: 'sign-in', component: SigninComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path : '**', component: ErrorPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
