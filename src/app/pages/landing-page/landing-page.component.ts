356e723d724800bb7b1f7b53d44b5c7cc75544a5
Paul Chesa <paulchesa1@gmail.com>
2022-11-11 06:12:47 +0300

    Added functionality to route to pages

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  goToCharacters(){
+    return this._router.navigate(['/characters']);
   }
 
 }
