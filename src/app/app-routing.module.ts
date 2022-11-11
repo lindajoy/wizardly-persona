24e978e8b969125f838e62bed4330c557b600a82
Paul Chesa <paulchesa1@gmail.com>
2022-11-11 06:11:09 +0300

    Added characters page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { SigninComponent } from './components/signin/signin.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { WizardlyCharactersComponent } from './pages/characters/characters.component';
 
 //Main routes module
 const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
+  { path: 'characters', component: WizardlyCharactersComponent},
   { path: 'sign-in', component: SigninComponent },
   { path: 'register-user', component: SignUpComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent },
